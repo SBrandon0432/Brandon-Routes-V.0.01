eac5a65117a587a3f544d45dafbc15ed31568b9c
sbrandon0432 <sbrandon0432@yahoo.com>
2021-12-09 20:36:11 -0800

    file spelled wrong

--- Client/src/App/Landing/Landing.tsx
+++ Client/src/App/Landing/Landing.tsx
@@ -1,4 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import './LandingS.scss';
+
+
+interface IProps {
+
+}
+
+
+
+
 
 
 const Landing: React.FC = () => {
--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,6 +1,7 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
-import Landing from './App/Landing/Landing'
+import Landing from './App/Landing/Landing';
+import './indexS.scss';
 
 ReactDOM.render (
 
