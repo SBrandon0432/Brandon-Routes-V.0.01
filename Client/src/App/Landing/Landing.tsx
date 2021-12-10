7cf3921cf80a43774f299f5ad07cd8e2d4130d33
sbrandon0432 <sbrandon0432@yahoo.com>
2021-12-09 20:29:36 -0800

    main file bones setup

---
+++ Client/src/App/Landing/Landing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Landing: React.FC = () => {
+
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Landing;
---
+++ Client/src/index.tsx
@@ -0,0 +1,16 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import Landing from './App/Landing/Landing'
+
+ReactDOM.render (
+
+  <StrictMode>
+    <div className='App'>
+      <Landing />
+    </div>
+
+  </StrictMode>,
+
+  document.getElementById('root')
+
+);
