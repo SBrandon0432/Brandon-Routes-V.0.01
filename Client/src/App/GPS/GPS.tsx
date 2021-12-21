56343a17a87af8a1d884e76de335db0c87b646c7
sbrandon0432 <sbrandon0432@yahoo.com>
2021-12-20 17:23:32 -0800

    mapbox api functinal

--- Client/src/App/GPS/GPS.tsx
+++ Client/src/App/GPS/GPS.tsx
@@ -3,14 +3,52 @@ import mapBoxToken  from '../../../../tokens'
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './GPSS.scss'
-import MapboxDirections from '@mapbox/';
+
+
 
 
 interface IProps {
 
 }
 
-// HTMLDivElement
+const useGps = () => {
+
+  const [result, setResult] = useState<any>({})
+
+  const start = [-122.662323, 45.523751];
+  const end = [-122.303707, 45.612333]
+
+  useEffect(()=> {
+
+      async function getGpsDirection() {
+        const query = await fetch(
+          `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+          { method: 'GET' }
+        );
+        const json = await query.json();
+        const data = json.routes[0];
+        const route = data.geometry.coordinates;
+        const geojson = {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: route
+          }
+        };
+        setResult({
+          data: data,
+          geojson: geojson
+        });
+        console.log(geojson)
+      }
+     getGpsDirection();
+
+  },[])
+
+  return result
+}
+
 
 const useMap = () => {
 
@@ -19,11 +57,14 @@ const useMap = () => {
   const [lng, setLng] = useState<number>(-70.9);
   const [lat, setLat] = useState<number>(42.35);
   const [zoom, setZoom] = useState<number>(9);
-
+  const [result, setResult] = useState<any>({})
+  const start = [-122.662323, 45.523751];
+  const end = [-122.303707, 45.612333]
 
     useEffect(() => {
       if (map.current) return;
       async function mapReq() {
+
         mapboxgl.accessToken = mapBoxToken;
         map.current = new mapboxgl.Map({
           container: mapContainer.current,
@@ -31,7 +72,6 @@ const useMap = () => {
           center: [lng, lat],
           zoom: zoom
         });
-
 
       }
       mapReq();
@@ -46,19 +86,96 @@ const useMap = () => {
         }))
 
         map.current.addControl(new mapboxgl.FullscreenControl({container: document.getElementById('root')}));
-        map.current.addControl(new mapboxgl   ({ accessToken: mapBoxToken}), 'top-left'  );
-        // MapboxDirections
-
 
     },[]);
 
-  return {mapContainer, map}
+    useEffect(() => {
+
+      async function getGpsDirection() {
+
+        const query = await fetch(
+          `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+          { method: 'GET' }
+        );
+
+        const json = await query.json();
+        const data = json.routes[0];
+        const route = data.geometry.coordinates;
+
+        const geojson = {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: route
+          }
+        };
+
+        setResult({
+          data: data,
+          geojson: geojson
+        });
+
+        console.log(geojson)
+      }
+     getGpsDirection();
+
+  },[])
+
+  // map.current.on('load', ()=> {
+
+  //   map.current.addLayer({
+  //     id: 'route',
+  //     type: 'line',
+  //     source: {
+  //       type: 'geojson',
+  //       data: result.geojson
+  //     },
+
+  //     layout: {
+  //       'line-join': 'round',
+  //       'line-cap': 'round'
+  //     },
+
+  //     paint: {
+  //       'line-color': '#3887be',
+  //       'line-width': 5,
+  //       'line-opacity': 0.75
+  //     }});
+
+  // });
+
+  return { mapContainer, map, result }
 }
 
 
 const GPS: React.FC = () => {
 
-  const {mapContainer, map} = useMap();
+  const {mapContainer, map, result} = useMap();
+
+  if (map.current) {
+    map.current.on('load', ()=> {
+      map.current.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: result.geojson
+        },
+
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
+        }});
+
+    });
+  }
 
 
   return (
